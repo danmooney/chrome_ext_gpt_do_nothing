74d7f79ece38da2ace8290e509fa57e0846a47e4
Dan Mooney <dan.mooney@mminc.co>
2012-10-19 20:26:29 -0400

    Fusioncash GPT klass

---
+++ js/Content/Gpt/FusionCash.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+    $$.klass(function GptTreasuretrooper () {
+        this.urlArr = [
+            {
+                url: 'http://www.fusioncash.net/offers.php*',
+                type: 'offers'
+            }
+        ];
+    }).inheritFrom('Gpt');
+}());
